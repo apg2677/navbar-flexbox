e1276c3cc490becee2bc44a0bf4de5bf76569abe
Amir <apourrezaei@gmail.com>
2023-05-23 14:35:06 -0400

    finished

--- src/DrawerComp.js
+++ src/DrawerComp.js
@@ -8,20 +8,43 @@ import {
   ListItemText,
 } from "@mui/material";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
-function DrawerComp() {
+function DrawerComp({ pages }) {
   const [openDrawer, setOpenDrawer] = useState(false);
   return (
     <>
       <Drawer open={openDrawer} onClose={() => setOpenDrawer(false)}>
         <List>
+          {pages.map((page, i) => {
+            return (
+              <ListItemButton
+                onClick={() => {
+                  setOpenDrawer(false);
+                }}
+                key={i}
+              >
+                <ListItemIcon>
+                  <ListItemText>{page}</ListItemText>
+                </ListItemIcon>
+              </ListItemButton>
+            );
+          })}
           <ListItemButton>
             <ListItemIcon>
               <ListItemText>Login</ListItemText>
             </ListItemIcon>
           </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <ListItemText>Sign Up</ListItemText>
+            </ListItemIcon>
+          </ListItemButton>
         </List>
       </Drawer>
-      <IconButton color="inherit" onClick={() => setOpenDrawer(!openDrawer)}>
+      <IconButton
+        sx={{ marginLeft: "auto" }}
+        color="inherit"
+        onClick={() => setOpenDrawer(!openDrawer)}
+      >
         <MenuRoundedIcon />
       </IconButton>
     </>
--- src/Header.js
+++ src/Header.js
@@ -8,10 +8,19 @@ import {
   Tab,
   Box,
   Button,
+  useMediaQuery,
+  useTheme,
 } from "@mui/material";
 import ShoppingCartCheckoutIcon from "@mui/icons-material/ShoppingCartCheckout";
+
+const PAGES = ["Products", "Services", "About Us", "Contact Us"];
+
 const Header = () => {
   const [value, setValue] = useState(0);
+  const theme = useTheme();
+  console.log("theme", theme);
+  const isMatch = useMediaQuery(theme.breakpoints.down("md"));
+  console.log("isMatch", isMatch);
   const handleChange = (e, val) => {
     console.log("e.target", val);
     setValue(val);
@@ -21,27 +30,35 @@ const Header = () => {
       <AppBar sx={{ background: "#063970" }}>
         <Toolbar>
           <ShoppingCartCheckoutIcon />
-
-          <Tabs
-            sx={{ marginLeft: "auto" }}
-            textColor="inherit"
-            onChange={handleChange}
-            value={value}
-            indicatorColor="secondary"
-          >
-            <Tab label="Products" />
-            <Tab label="Services" />
-            <Tab label="Contact Us" />
-            <Tab label="About Us" />
-          </Tabs>
-          <Button sx={{ marginLeft: "auto" }} variant="contained">
-            Login
-          </Button>
-          <Button sx={{ marginLeft: 2 }} variant="contained">
-            Sign Up
-          </Button>
+          {isMatch ? (
+            <>
+              <Typography sx={{ fontSize: 24, marginLeft: "1rem" }}>
+                SHOPPEE
+              </Typography>
+              <DrawerComp pages={PAGES} />
+            </>
+          ) : (
+            <>
+              <Tabs
+                sx={{ marginLeft: "auto" }}
+                textColor="inherit"
+                onChange={handleChange}
+                value={value}
+                indicatorColor="secondary"
+              >
+                {PAGES.map((page, i) => {
+                  return <Tab key={i} label={page} />;
+                })}
+              </Tabs>
+              <Button sx={{ marginLeft: "auto" }} variant="contained">
+                Login
+              </Button>
+              <Button sx={{ marginLeft: 2 }} variant="contained">
+                Sign Up
+              </Button>
+            </>
+          )}
         </Toolbar>
-        <DrawerComp />
       </AppBar>
     </>
   );
